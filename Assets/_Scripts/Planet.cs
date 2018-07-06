190655d9f4563ca9e00772597491d175e56c7fb0
Sponglee <acearutyunyan@gmail.com>
2018-07-06 23:10:49 +0300

    06.07.18
    DEEP REVERT

--- Assets/_Scripts/Planet.cs
+++ Assets/_Scripts/Planet.cs
@@ -188,13 +188,8 @@ public class Planet : MonoBehaviour {
                 Vector3 goVel = new Vector3((float)goRb.Velocity.x, (float)goRb.Velocity.y, (float)goRb.Velocity.z);
                 float angleOfAttack = Vector3.Dot(goVel, go.transform.parent.position - transform.position);
 
-
-<<<<<<< HEAD
                 if (diff < distance /*&& angleOfAttack < 0 && goVel.sqrMagnitude > fastestAsteroidSpeed && diff <= range*/)
 
-=======
-                if (diff < distance /*&& angleOfAttack < 0 && goVel.sqrMagnitude > fastestAsteroidSpeed */&& diff <= range)
->>>>>>> parent of e588012... 06.07.18
                 {
 
                     distance = diff;
